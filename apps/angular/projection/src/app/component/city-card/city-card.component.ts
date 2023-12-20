be1a2f7be3d4a719569e1b01ad6c95ce5935ad32
Muhammad Ishaan <iftekharctgbd@gmail.com>
2023-12-20 23:45:39 +0600

    refactor(card): projection challenge completed

--- apps/angular/projection/src/app/component/city-card/city-card.component.ts
+++ apps/angular/projection/src/app/component/city-card/city-card.component.ts
@@ -1,13 +1,45 @@
 import { Component, OnInit } from '@angular/core';
+import { CityStore } from '../../data-access/city.store';
+import {
+  FakeHttpService,
+  randomCity,
+} from '../../data-access/fake-http.service';
+import { City } from '../../model/city.model';
+import { CardComponent } from '../../ui/card/card.component';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
   selector: 'app-city-card',
-  template: 'TODO City',
+  template: `
+    <app-card [list]="cities" (add)="onCityAdd()" class="bg-light-red">
+      <img src="assets/img/city.png" width="200px" />
+      <ng-template #rowRef let-city>
+        <app-list-item (delete)="onCityDelete(city.id)">
+          {{ city.name }}
+        </app-list-item>
+      </ng-template>
+    </app-card>
+  `,
   standalone: true,
-  imports: [],
+  imports: [CardComponent, ListItemComponent],
 })
 export class CityCardComponent implements OnInit {
-  constructor() {}
+  cities: City[] = [];
+  constructor(
+    private http: FakeHttpService,
+    private store: CityStore,
+  ) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.http.fetchCities$.subscribe((t) => this.store.addAll(t));
+
+    this.store.cities$.subscribe((t) => (this.cities = t));
+  }
+  onCityDelete(id: number): void {
+    this.store.deleteOne(id);
+  }
+
+  onCityAdd(): void {
+    this.store.addOne(randomCity());
+  }
 }
--- apps/angular/projection/src/app/component/student-card/student-card.component.ts
+++ apps/angular/projection/src/app/component/student-card/student-card.component.ts
@@ -1,16 +1,29 @@
 import { Component, OnInit } from '@angular/core';
-import { FakeHttpService } from '../../data-access/fake-http.service';
+import {
+  FakeHttpService,
+  randStudent,
+} from '../../data-access/fake-http.service';
 import { StudentStore } from '../../data-access/student.store';
 import { CardType } from '../../model/card.model';
 import { Student } from '../../model/student.model';
 import { CardComponent } from '../../ui/card/card.component';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
   selector: 'app-student-card',
-  template: `<app-card
-    [list]="students"
-    [type]="cardType"
-    customClass="bg-light-green"></app-card>`,
+  template: `
+    <app-card
+      [list]="students"
+      (add)="onStudentAdd()"
+      customClass="bg-light-green">
+      <img src="assets/img/student.webp" width="200px" />
+      <ng-template #rowRef let-student>
+        <app-list-item (delete)="onStudentDelete(student.id)">
+          {{ student.firstName }}
+        </app-list-item>
+      </ng-template>
+    </app-card>
+  `,
   standalone: true,
   styles: [
     `
@@ -19,17 +32,28 @@ import { CardComponent } from '../../ui/card/card.component';
       }
     `,
   ],
-  imports: [CardComponent],
+  imports: [CardComponent, ListItemComponent],
 })
 export class StudentCardComponent implements OnInit {
   students: Student[] = [];
   cardType = CardType.STUDENT;
 
-  constructor(private http: FakeHttpService, private store: StudentStore) {}
+  constructor(
+    private http: FakeHttpService,
+    private store: StudentStore,
+  ) {}
 
   ngOnInit(): void {
     this.http.fetchStudents$.subscribe((s) => this.store.addAll(s));
 
     this.store.students$.subscribe((s) => (this.students = s));
+  }
+
+  onStudentDelete(id: number): void {
+    this.store.deleteOne(id);
+  }
+
+  onStudentAdd(): void {
+    this.store.addOne(randStudent());
   }
 }
